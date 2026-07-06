9173ea0f29f85391bccec14b56ab890cd627c8ea
Amel Dib <dibamel95@gmail.com>
2026-07-06 16:58:20 +0100

    Add back-to-top button functionality

--- ShopEase.js
+++ ShopEase.js
@@ -96,3 +96,18 @@ function showToast() {
     toast.classList.remove("show");
   }, 2000);
 }
+window.onscroll = function () {
+  const btn = document.getElementById("backToTop");
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+document.getElementById("backToTop").onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
