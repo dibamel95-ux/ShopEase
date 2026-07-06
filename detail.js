80f2a02e731656601f86a9a5ed0d9e5e7b1d0091
Amel Dib <dibamel95@gmail.com>
2026-07-06 17:02:20 +0100

    Update detail.js

--- detail.js
+++ detail.js
@@ -84,3 +84,18 @@ let mainPage = document.querySelector(`.logo`);
 mainPage.addEventListener("click", () => {
   window.location.href = `index.html`;
 });
+window.onscroll = function () {
+  const btn = document.getElementById("backToTop");
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+document.getElementById("backToTop").onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
