b65c42e73b3cd0e8dd90d7f4e82df1abb207efd9
Amel Dib <dibamel95@gmail.com>
2026-07-22 23:36:58 +0100

    Improve cart functionality and total price display
    Refactor cart item handling and total price calculation.

--- cart.js
+++ cart.js
@@ -1,7 +1,9 @@
 let currentUser = JSON.parse(localStorage.getItem(`currentUser`));
-let cartItem = JSON.parse(localStorage.getItem(`cartItems_${currentUser.id}`)) || [];
+let cartItem =
+  JSON.parse(localStorage.getItem(`cartItems_${currentUser.id}`)) || [];
 let cartProduct = [];
 async function getData() {
+  cartProduct = [];
   for (let item of cartItem) {
     let res = await fetch(`https://dummyjson.com/products/${item.id}`);
     let data = await res.json();
@@ -16,6 +18,9 @@ getData();
 function display(arr) {
   let container = document.getElementById(`container`);
   container.innerHTML = "";
+
+  let result = 0; // حساب المجموع الكلي خارج اللوب لتفادي تكراره خطأ
+
   arr.forEach((prod) => {
     container.innerHTML += ` <tr>
   <td> <img src="${prod.thumbnail}"></td>
@@ -29,13 +34,14 @@ function display(arr) {
   <td><p>Total: ${prod.price * prod.quantity}</p></td>
   <td><button class="delete" onclick="deleteProduct(${prod.id})"> <span class="material-symbols-outlined">delete</span></button></td>
   </tr>`;
-    let cartTotal = document.getElementById("totalPrice");
-    let result = 0;
-    arr.forEach((product) => {
-      result += product.price * product.quantity;
-    });
-    cartTotal.textContent = `$${result.toFixed(2)}`;
+
+    result += prod.price * prod.quantity;
   });
+
+  let cartTotal = document.getElementById("totalPrice");
+  if (cartTotal) {
+    cartTotal.textContent = `$${result.toFixed(2)}`;
+  }
 }
 function increaseQty(id) {
   let item = cartItem.find((p) => p.id === id);
@@ -60,25 +66,12 @@ function decreaseQty(id) {
 function deleteProduct(id) {
   let confirmDelete = confirm("Are you sure you want to delete this product?");
   if (confirmDelete) {
-  cartProduct = cartProduct.filter((product) => product.id !== id);
-  cartItem = cartItem.filter((item) => item.id !== id);
-  localStorage.setItem("cartItems", JSON.stringify(cartItem));
-  let cartTotal = document.getElementById("totalPrice");
-  cartTotal.innerHTML = "0$";
-  display(cartProduct) }
-}
-window.onscroll = function () {
-  const btn = document.getElementById("backToTop");
-  if (
-    document.body.scrollTop > 300 ||
-    document.documentElement.scrollTop > 300
-  ) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
+    cartProduct = cartProduct.filter((product) => product.id !== id);
+    cartItem = cartItem.filter((item) => item.id !== id);
+    localStorage.setItem(
+      `cartItems_${currentUser.id}`,
+      JSON.stringify(cartItem),
+    );
+    display(cartProduct);
   }
-};
-
-document.getElementById("backToTop").onclick = function () {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
+}
